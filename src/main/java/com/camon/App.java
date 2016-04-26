613aea81f1d72ef3f34c640249ff42a4f4928483
Jooyong Sung <camon85@gmail.com>
2016-04-26 17:10:39 +0900

    특정 job 수동 실행

--- src/main/java/com/camon/App.java
+++ src/main/java/com/camon/App.java
@@ -1,14 +1,37 @@
 package com.camon;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * Created by user on 2016-03-31.
  */
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        SpringApplication app = new SpringApplication(App.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        ConfigurableApplicationContext ctx = app.run(args);
+
+        Job txtToDatabaseJob = ctx.getBean("txtToDatabaseJob", Job.class);
+        JobParameters defaultJobParameters = new JobParametersBuilder().toJobParameters();
+//        JobParameters dateJobParameters = new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
+
+        JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+        JobExecution jobExecution = jobLauncher.run(txtToDatabaseJob, defaultJobParameters);
+        BatchStatus status = jobExecution.getStatus();
+        log.info("getStatus -> {}", status);
     }
 }
